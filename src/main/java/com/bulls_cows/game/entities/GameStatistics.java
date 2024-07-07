1f6f6f69b4b22a3bd9acf9362b1e4ff695ac27fd
yxngxr1 <gderganov@yandex.ru>
2024-07-07 22:59:50 +0400

    Added statistics view

--- src/main/java/com/bulls_cows/game/entities/GameStatistics.java
+++ src/main/java/com/bulls_cows/game/entities/GameStatistics.java
@@ -9,27 +9,13 @@ public class GameStatistics
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private int attempts;
-    private long completionTime;
+    private Integer attempts;
+    private Long completionTime;
+    private Integer maxAttempts;
+    private Long maxCompletionTime;
 
     @ManyToOne
     private User user;
-
-    public int getAttempts() {
-        return attempts;
-    }
-
-    public void setAttempts(int attempts) {
-        this.attempts = attempts;
-    }
-
-    public long getCompletionTime() {
-        return completionTime;
-    }
-
-    public void setCompletionTime(long completionTime) {
-        this.completionTime = completionTime;
-    }
 
     public Long getId() {
         return id;
@@ -37,5 +23,55 @@ public class GameStatistics
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Integer getAttempts() {
+        return attempts;
+    }
+
+    public void setAttempts(int attempts) {
+        this.attempts = attempts;
+    }
+
+    public Long getCompletionTime() {
+        return completionTime;
+    }
+
+    public void setCompletionTime(long completionTime) {
+        this.completionTime = completionTime;
+    }
+
+    public Integer getMaxAttempts() {
+        return maxAttempts;
+    }
+
+    public void setMaxAttempts(Integer maxAttempts) {
+        this.maxAttempts = maxAttempts;
+    }
+
+    public Long getMaxCompletionTime() {
+        return maxCompletionTime;
+    }
+
+    public void setMaxCompletionTime(Long maxCompletionTime) {
+        this.maxCompletionTime = maxCompletionTime;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "GameStatistics{" +
+                "id=" + id +
+                ", attempts=" + attempts +
+                ", completionTime=" + completionTime +
+                ", user=" + (user != null ? user.getUsername() : "null") +
+                '}';
     }
 }
--- src/main/java/com/bulls_cows/game/service/GameStatService.java
+++ src/main/java/com/bulls_cows/game/service/GameStatService.java
@@ -1,10 +1,44 @@
 package com.bulls_cows.game.service;
 
+import com.bulls_cows.game.entities.GameStatistics;
+import com.bulls_cows.game.entities.User;
 import com.bulls_cows.game.repository.GameStatRepository;
+import com.bulls_cows.game.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class GameStatService
 {
-    GameStatRepository repository;
+    @Autowired
+    GameStatRepository gameStatRepository;
 
+    @Autowired
+    private UserRepository userRepository;
 
+    public GameStatService(GameStatRepository gameStatRepository)
+    {
+        this.gameStatRepository = gameStatRepository;
+    }
+
+    public List<GameStatistics> findAllByUserId(Long userId) {
+        return gameStatRepository.findAllByUserId(userId);
+    }
+
+    public void saveGameStat(String username, GameStatistics gameStatistics) {
+        User user = userRepository.findByUsername(username);
+
+        if (user != null) {
+            GameStatistics gameStatistics1 = new GameStatistics();
+            gameStatistics1.setAttempts(gameStatistics.getAttempts());
+            gameStatistics1.setCompletionTime(gameStatistics.getCompletionTime());
+            gameStatistics1.setMaxAttempts(gameStatistics.getMaxAttempts());
+            gameStatistics1.setMaxCompletionTime(gameStatistics.getMaxCompletionTime());
+            gameStatistics1.setUser(user);
+
+            gameStatRepository.save(gameStatistics1);
+        }
+    }
 }
